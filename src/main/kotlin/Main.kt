7133866669c55d0044ac5c2ef4ea709e96b55b81
orishaba-mercy <orishabamercybanga@gmail.com>
2023-02-03 11:35:44 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var  name = "orishaba mercy"
+    println(name)
+    var age = 23
+    println(age)
+    var phonenumber = +256787930683
+    println(phonenumber)
+    var weight = "67kgs"
+    println(weight)
+    var isKenyan = false
+    println(isKenyan)
+
+
+
+
+}
